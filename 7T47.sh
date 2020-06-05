6c00f354d3649ff631b57835bb9a6a4ae644c2d3
brenmiester <samuelsson.benjamin01@gmail.com>
2020-06-05 22:42:08 +0800

    Clean-up and Efficiency

--- 7T47.sh
+++ 7T47.sh
@@ -1,19 +1,30 @@
 #!/bin/sh
 
-date='%d'
+#Creates Time and Date variables in correct timezone and format
+timecode=$(TZ="Australia/Hobart" date +%Y%m%d_%H:%M)
+date=$(date +%Y_%m_%d)
 
-# URL of Icecast/Shoutcast stream
+#Output directory
+output_dir=/home/pi/FTP/logger/7T47_PulseFM/$date
+
+if [ ! -d $output_dir ]; then
+mkdir $output_dir
+fi
+
+#URL and output filename
 url=https://stream.lava.net.au/au_tas_7T47_pulsefm
-output_filename=7T47_${date}
+output_filename=7T47_$timecode
 
-# Length in Seconds
+#Duration in Seconds
 duration=600
-
-#Output Directory
-output_dir=/home/pi/FTP/logger/7T47_PulseFM
 
 cd $output_dir
 
 streamripper $url -d $output_dir -l $duration -a $output_filename -o always
 
-mv *.cue Meta
+#Moves Meta Files
+if [ ! -d Meta ]; then
+mkdir Meta
+fi
+
+mv *.cue $output_dir/Meta
