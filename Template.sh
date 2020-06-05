288966258f27b13f63e0f7749d5114093e46a884
brenmiester <samuelsson.benjamin01@gmail.com>
2020-06-05 22:45:36 +0800

    Create Template.sh

---
+++ Template.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#Creates Time and Date variables in correct timezone and format
+timecode=$(TZ="###TIMEZONE OF BROADCAST###" date +%Y%m%d_%H:%M)
+date=$(date +%Y_%m_%d)
+
+#Output directory
+output_dir=###OUTPUT DIR###
+
+if [ ! -d $output_dir ]; then
+mkdir $output_dir
+fi
+
+#URL and output filename
+#Needs direct URL, can be found via network logs
+url=###STREAM URL###
+output_filename=###CALLSIGN###_$timecode
+
+#Duration in Seconds
+duration=###DURATION###
+
+cd $output_dir
+
+streamripper $url -d $output_dir -l $duration -a $output_filename -o always
+
+#Moves Meta Files
+if [ ! -d Meta ]; then
+mkdir Meta
+fi
+
+mv *.cue $output_dir/Meta
